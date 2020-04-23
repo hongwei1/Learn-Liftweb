e9ed6223ed46a96dd61721c6c62de0ade2520df0
hongwei <hongwei@tesobe.com>
2020-04-23 12:20:36 +0200

    chapter2.11 Invoking server-side functions from the client-AJax

--- src/main/scala/lift/cookbook/snippet/JsCommand.scala
+++ src/main/scala/lift/cookbook/snippet/JsCommand.scala
@@ -1,10 +1,14 @@
 package lift.cookbook.snippet
 
 import net.liftweb.util.BindHelpers._
-import net.liftweb.http.js.JsCmds.{JsCrVar, Script}
+import net.liftweb.http.js.{JsCmd, JsCmds}
+import net.liftweb.http.js.JsCmds._
 import net.liftweb.http.js.JE.{JsFunc, JsRaw}
-import net.liftweb.http.js.JsCmds
-import xml.Text
+import net.liftweb.http.SHtml
+import net.liftweb.json._
+
+import scala.xml.Text
+
 
 object JsCommand {
 
@@ -23,7 +27,7 @@ object JsCommand {
 //    val command4 = JsCmds.Alert("Alert sent from Lift")
     val command4 = JsCmds.Run("")
     
-    
+     
     val command5 = confirm
     
     
@@ -47,5 +51,62 @@ object JsCommand {
     JsRaw(jsFunc).cmd
 
   }
+  
+  
+  
+  //Ajax Example
+  implicit val formats = net.liftweb.json.DefaultFormats
+
+//  var persons: List[String] = "Carly" :: "Joe" :: "John" :: "Mary" :: Nil
+
+  def loadData: JsCmd = {
+
+    //Get the json from the browser, and remove it in the js
+    def funcBody(json: JValue) = { 
+      val p = json.extract[String]
+//      persons = persons.filterNot(_ == p)
+
+//      we can do complex here, database or business logic here. 
+    //<select id="person">
+    //  <option value="Carly">Carly</option>
+    //  <option value="Joe">Joe</option>
+    //  <option value="John">John</option>
+    //  <option value="Mary">Mary</option>
+    //</select>
+      //("#person  [value='John']").remove() will delete the `<option value="John">John</option>` -->
+      //      <select id="person">
+      //        <option value="Carly">Carly</option>
+      //        <option value="Joe">Joe</option>
+      //        <option value="Mary">Mary</option>
+      //      </select>
+      
+      JsRaw( 
+        """ $("#person [value='""" + p + """']").remove() """).cmd & 
+        JsCmds.Alert(p + " was removed")
+
+    }
+    
+    //<script type="text/javascript">
+    //// <![CDATA[
+    //function ajaxDeletePeople(person) {
+    //    liftAjax.lift_ajaxHandler('F7625200364944RMKZ3=' + encodeURIComponent(JSON.stringify(person)), null, null, null);
+    //    }
+    //
+    //// ]]>
+    //</script>
+    
+    Function(
+      "ajaxDeletePeople", // "ajaxDeletePeople1" if set the different name here. You will see the error from the `Console`.
+      List("person"),
+      SHtml.jsonCall(
+        JsRaw("person"),  //the value to be sent to the serve
+        (value: JValue) => funcBody(value) //the function to call when the data is sent
+      )
+    )
+
+  }
+
+  def jsFunction = { "*" #> Script(loadData) }
 
 }
+
